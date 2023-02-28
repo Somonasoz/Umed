dd660cda0f43b7d14ff2e0ef4919df05d87ffc7b
Somonasoz <mirzoboshievumed@mail.ru>
2023-02-28 22:22:17 +0500

    Progect ready for deployment

---
+++ main.py
@@ -0,0 +1,46 @@
+from tkinter import Menu
+
+from aiosmtplib.__main__ import port
+from flask import Flask, render_template, url_for, Response
+from flask import request
+import speedtest
+import json
+
+app = Flask(__name__)
+
+
+def speed_test():
+    servers = [3212, 6699, 5243, 7575, 3783, 6883, 5354, 3402]
+    threads = None
+    s = speedtest.Speedtest()
+    try:
+        s.get_servers(servers)
+        
+    except Exception as e:
+        return json.dumps({'status': 'error', 'message': 'An error occured. Please, try later'}, separators=(',', ':'))
+    
+    s.get_best_server()
+    s.download(threads = threads)
+    s.upload(threads = threads)
+    s.results.share()
+
+    results_dict = s.results.dict()
+
+    result = json.dumps({
+            'status': 'success',
+            'download': f"{results_dict['download'] / 1024 / 1024:.2f}", 
+            'upload': f"{results_dict['upload'] / 1024 / 1024:.2f}", 
+            'ping': f"{results_dict['ping']:.2f}"
+        }, separators=(',', ':'))
+    
+    return result
+
+@app.route("/")
+def test_page():
+    return render_template('test_page.html', site_title='Спид Тест')
+
+@app.route("/api")
+def api():
+    return speed_test()
+
+app.run(host='0.0.0.0', port=5000)
